e7c516fde07efb94bcbdefbbd33f379d9c78ebc1
Daniel Fries <daniel.w.fries@gmail.com>
2020-03-14 14:47:09 -0400

    intial migration to 1.0

--- dreamlord.py
+++ dreamlord.py
@@ -15,20 +15,20 @@ game = discord.Game(name="github.com/cameronleong", url="github.com/cameronleong
 async def on_message(message):
 	if message.author == client.user:			# we do not want the bot to reply to itself
 		return
-		
+
 	if message.content.startswith('!hello'):
 		msg = 'Greetings {0.author.mention}'.format(message)
 		await client.send_message(message.channel, msg)
-	
+
 	if message.content.startswith('!avalon'):
 		if message.channel in busyChannels:
 			await client.send_message(message.channel, "Channel busy with another activity.")
 		else:
 			busyChannels.append(message.channel)
-			await client.send_message(message.channel, "Starting **The Resistance: Avalon - Discord Edition** in `#"+message.channel.name+"`...")
+			await message.channel.send("Starting **The Resistance: Avalon - Discord Edition** in `#"+message.channel.name+"`...")
 			await avalon(client, message)
 			busyChannels.remove(message.channel)
-	
+
 	"""
 	if message.content.startswith('!word'):
 		if message.channel in busyChannels:
@@ -38,20 +38,21 @@ async def on_message(message):
 			await client.send_message(message.channel, "Starting **Guess-the-Word** in `#"+message.channel.name+"`...")
 			await run(client, message)
 			busyChannels.remove(message.channel)
-	"""		
+	"""
 	#if message.content.startswith('!score'):
 	#	await scoreboard(client, message)
 
 	if message.content.startswith('!help'):
-		await client.send_message(message.author, 'Please visit https://cameronleong.github.io/avalon to find out more.')
-		
-		
+		# message.channel.send()
+		await message.author.send('Please visit https://cameronleong.github.io/avalon to find out more.')
+
+
 @client.event
 async def on_ready():
 	print('Connected!')
 	print('Username: ' + client.user.name)
-	print('ID: ' + client.user.id)
-	await client.change_status(game)
-	
+	print('ID: ' + str(client.user.id))
+	await client.change_presence(activity = game)
+
 
 client.run('YOUR TOKEN HERE')
