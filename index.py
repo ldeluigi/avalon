2873946d56c29c372fdf97e7e522bccb8714238f
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-10-21 13:33:02 +0200

    Add HEAD answer

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from socketserver import ThreadingMixIn
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
 
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(s):
         s.send_response(200)
@@ -10,12 +11,20 @@ class Handler(BaseHTTPRequestHandler):
         s.end_headers()
         s.wfile.write(bytes("Hello World!", "UTF-8"))
 
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/plain")
+        s.end_headers()
+
+
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
     daemon_threads = True
+
 
 def serve_on_port(port):
     server = ThreadingHTTPServer(("", int(port)), Handler)
     server.serve_forever()
+
 
 port = os.getenv("PORT") if os.getenv("PORT") is not None else 80
 
